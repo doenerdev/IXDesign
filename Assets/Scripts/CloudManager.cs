1a9f91729386c80a7095ccf08e450e93d84d6afa
Andreas Hoch <Andreas_Hoch@gmx.de>
2016-12-21 10:51:02 +0100

    models + sun cycle

---
+++ Assets/Scripts/CloudManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CloudDirection
+{
+    Left,
+    Right
+}
+
+public class CloudManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] _cloudTempaltes;
+    [SerializeField] private GameObject _cloudWrapper;
+    [SerializeField] private CloudDirection _cloudDirection;
+    [SerializeField] [Range(0f, 2f)] private float _cloudMovementSpeed;
+    [SerializeField] private Vector3 _lowerLimits;
+    [SerializeField] private Vector3 _upperLimits;
+    [SerializeField] private int _minInitalCloudQty;
+    [SerializeField] private int _maxInitalCloudQty;
+
+    private List<GameObject> _clouds;
+
+    private void Awake()
+    {
+        _clouds = new List<GameObject>();
+    }
+
+	private void Start ()
+	{
+	    int cloudQty = Random.Range(_minInitalCloudQty, _maxInitalCloudQty+1);
+	    for (int i = 0; i < cloudQty; i++)
+	    {
+	        InitializeCloud();
+	    }
+	}
+
+
+    private void InitializeCloud()
+    {
+        float posX = _cloudDirection == CloudDirection.Left ? _lowerLimits.x : _upperLimits.x;
+        Vector3 initPosition = new Vector3(posX, Random.Range(_lowerLimits.y, _upperLimits.y), Random.Range(_lowerLimits.z, _upperLimits.z));
+        GameObject cloud = GameObject.Instantiate(_cloudTempaltes[Random.Range(0, _cloudTempaltes.Length-1)], initPosition, Quaternion.identity);
+        cloud.transform.SetParent(_cloudWrapper.transform);
+         _clouds.Add(cloud);
+    }
+
+    private void MoveClouds()
+    {
+        int direction = _cloudDirection == CloudDirection.Left ? 1 : -1;
+        foreach (GameObject cloud in _clouds)
+        {
+            cloud.transform.position = new Vector3(transform.position.x + _cloudMovementSpeed * direction, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void RemoveClouds()
+    {
+        float limitX = _cloudDirection == CloudDirection.Left ? _upperLimits.x : _lowerLimits.x;
+        foreach (GameObject cloud in _clouds)
+        {
+            if (_cloudDirection == CloudDirection.Left && cloud.transform.position.x > _upperLimits.x)
+            {
+                _clouds.Remove(cloud);
+                Destroy(cloud);
+            }
+            else if (_cloudDirection == CloudDirection.Right && cloud.transform.position.x < _lowerLimits.x)
+            {
+                _clouds.Remove(cloud);
+                Destroy(cloud);
+            }
+        }
+    }
+
+	void Update () {
+		MoveClouds();
+	    RemoveClouds();
+	}
+}
--- Assets/Scripts/SunCycle.cs
+++ Assets/Scripts/SunCycle.cs
@@ -9,33 +9,222 @@ public enum SunCycleDirection
     CounterClockwise
 }
 
+public enum TimeOfDay
+{
+    Dusk,
+    Day,
+    Dawn,
+    Night
+}
+
+[RequireComponent(typeof(Light))]
 public class SunCycle : MonoBehaviour
 {
-    [SerializeField] private Color _morningBackgroundColor;
+    [SerializeField] private Color _duskBackgroundColor;
     [SerializeField] private Color _dayBackgroundColor;
     [SerializeField] private Color _dawnBackgroundColor;
     [SerializeField]private Color _nightBackgroundColor;
 
-    [SerializeField] [Range(0f, 0.5f)] private float _cycleSpeed;
+    [SerializeField] private Color _duskAmbientColor;
+    [SerializeField] private Color _dayAmbientColor;
+    [SerializeField] private Color _dawnAmbientColor;
+    [SerializeField] private Color _nightAmbientColor;
+
+    [SerializeField]
+    [Range(0, 360)]
+    private float _duskSunXAngle;
+    [SerializeField]
+    [Range(0, 360)]
+    private float _daySunXAngle;
+    [SerializeField]
+    [Range(0, 360)]
+    private float _dawnSunXAngle;
+    [SerializeField]
+    [Range(0, 360)]
+    private float _nightSunXAngle;
+    [SerializeField]
+    [Range(0, 360)]
+    private float _nightSunXAngle2Dusk;
+
+    [SerializeField] [Range(0, 360)] private int _cycleLength = 36;
     [SerializeField] private SunCycleDirection _cycleDirection;
     [SerializeField] [Range(0f, 360f)] private float _maxDeltaDaylight;
-    [SerializeField] private Material backgroundMaterial;
+    [SerializeField] private Material _backgroundMaterial;
+    [SerializeField] private TimeOfDay _intialTimeOfDay;
+    [SerializeField] [Range(0f, 1f)] private float _dayLightIntensity;
+    [SerializeField] [Range(0f, 1f)] private float _nightLightIntensity;
+    [SerializeField] [Range(0f, 1f)] private float _dawnLightIntensity;
+    [SerializeField] [Range(0f, 1f)] private float _duskLightIntensity;
 
+    private Light _sun;
     private float _initalSunRotation;
+    private TimeOfDay _timeOfDay = TimeOfDay.Day;
+    private float _SinglePhaseLength;
+    private int _cycleTimer = 0;
+    private float _cycleTimer_Ticks = 0f;
+    private Color _currentAmbientColor;
+    private float _lerpTimer = 0f;
 
     private void Awake()
     {
+        _sun = GetComponent<Light>();
         _initalSunRotation = transform.rotation.eulerAngles.x;
+        _SinglePhaseLength = _cycleLength/4;
+        InitializeCycle();
+
+   
+    }
+
+    private void InitializeCycle()
+    {
+        if (_intialTimeOfDay == TimeOfDay.Dusk)
+        {
+            InitDusk();
+        }
+        else if (_intialTimeOfDay == TimeOfDay.Day)
+        {
+            InitDay();
+        }
+        else if (_intialTimeOfDay == TimeOfDay.Dawn)
+        {
+            InitDawn();
+        }
+        else
+        {
+           InitNight();
+        }
+
+        _cycleTimer = Mathf.RoundToInt(_SinglePhaseLength);
+        Debug.Log(_SinglePhaseLength);
     }
 	
 	private void Update ()
 	{
-	    int direction = _cycleDirection == SunCycleDirection.Clockwise ? -1 : 1;
-
-	    
-
-	    transform.Rotate(_cycleSpeed * direction, 0, 0);
+	    UpdateTimers();
+        UpdateTimeOfDay();
 	}
+
+    private void UpdateTimers()
+    {
+        _cycleTimer_Ticks += Time.deltaTime;
+        if (Mathf.RoundToInt(_cycleTimer_Ticks % 60) >= 1)
+        {
+            _cycleTimer_Ticks = 0f;
+            _cycleTimer++;
+        }
+
+        if (_cycleTimer > _cycleLength)
+        {
+            _cycleTimer = 0;
+            _cycleTimer_Ticks = 0f;
+        }
+ 
+        _lerpTimer += (Time.deltaTime/(_SinglePhaseLength * (_cycleLength/100f)));
+    }
+
+    private void UpdateTimeOfDay()
+    {
+
+        if (_cycleTimer >= (int)TimeOfDay.Night * _SinglePhaseLength)
+        {
+            if (_timeOfDay != TimeOfDay.Night)
+            {
+                InitNight();
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(_nightSunXAngle2Dusk, transform.rotation.y, transform.rotation.z), Quaternion.Euler(360+_duskSunXAngle, transform.rotation.y, transform.rotation.z), _lerpTimer);
+                _sun.intensity = Mathf.Lerp(_nightLightIntensity, _duskLightIntensity, _lerpTimer);
+                RenderSettings.ambientLight = Color.Lerp(_nightAmbientColor, _duskAmbientColor, _lerpTimer);
+                _backgroundMaterial.color = Color.Lerp(_nightBackgroundColor, _duskBackgroundColor, _lerpTimer);
+            }
+        }
+        else if (_cycleTimer >= (int)TimeOfDay.Dawn * _SinglePhaseLength)
+        {
+            if (_timeOfDay != TimeOfDay.Dawn)
+            {
+                InitDawn();
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(_dawnSunXAngle, transform.rotation.y, transform.rotation.z), Quaternion.Euler(_nightSunXAngle, transform.rotation.y, transform.rotation.z), _lerpTimer);
+                _sun.intensity = Mathf.Lerp(_dawnLightIntensity, _nightLightIntensity, _lerpTimer);
+                RenderSettings.ambientLight = Color.Lerp(_dawnAmbientColor, _nightAmbientColor, _lerpTimer);
+                _backgroundMaterial.color = Color.Lerp(_dawnBackgroundColor, _nightBackgroundColor, _lerpTimer);
+            }
+        }
+        else if (_cycleTimer >= (int)TimeOfDay.Day * _SinglePhaseLength)
+        {
+            if (_timeOfDay != TimeOfDay.Day)
+            {
+                InitDay();
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(_daySunXAngle, transform.rotation.y, transform.rotation.z), Quaternion.Euler(_dawnSunXAngle, transform.rotation.y, transform.rotation.z), _lerpTimer);
+                _sun.intensity = Mathf.Lerp(_dayLightIntensity, _dawnLightIntensity, _lerpTimer);
+                RenderSettings.ambientLight = Color.Lerp(_dayAmbientColor, _dawnAmbientColor, _lerpTimer);
+                _backgroundMaterial.color = Color.Lerp(_dayBackgroundColor, _dawnBackgroundColor, _lerpTimer);
+            }
+        }
+        else
+        {
+            if (_timeOfDay != TimeOfDay.Dusk)
+            {
+                InitDusk();
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(_duskSunXAngle, transform.rotation.y, transform.rotation.z), Quaternion.Euler(_daySunXAngle, transform.rotation.y, transform.rotation.z), _lerpTimer);
+                _sun.intensity = Mathf.Lerp(_duskLightIntensity, _dayLightIntensity, _lerpTimer);
+                RenderSettings.ambientLight = Color.Lerp(_duskAmbientColor, _dayAmbientColor, _lerpTimer);
+                _backgroundMaterial.color = Color.Lerp(_duskBackgroundColor, _dayBackgroundColor, _lerpTimer);
+            }
+        }
+    }
+
+    private void InitDusk()
+    {
+        _timeOfDay = TimeOfDay.Dusk;
+        RenderSettings.ambientLight = _duskAmbientColor;
+        _backgroundMaterial.color = _duskBackgroundColor;
+        _currentAmbientColor = _duskAmbientColor;
+        _sun.intensity = _duskLightIntensity;
+        _lerpTimer = 0f;
+    }
+
+    private void InitDay()
+    {
+        _timeOfDay = TimeOfDay.Day;
+        RenderSettings.ambientLight = _dayAmbientColor;
+        _backgroundMaterial.color = _dayBackgroundColor;
+        _currentAmbientColor = _dayAmbientColor;
+        _sun.intensity = _dayLightIntensity;
+        _lerpTimer = 0f;
+    }
+
+    private void InitDawn()
+    {
+        _timeOfDay = TimeOfDay.Dawn;
+        RenderSettings.ambientLight = _dawnAmbientColor;
+        _backgroundMaterial.color = _dawnBackgroundColor;
+        _currentAmbientColor = _dawnAmbientColor;
+        _sun.intensity = _dawnLightIntensity;
+        _lerpTimer = 0f;
+    }
+
+    private void InitNight()
+    {
+        _timeOfDay = TimeOfDay.Night;
+        RenderSettings.ambientLight = _nightAmbientColor;
+        _backgroundMaterial.color = _nightBackgroundColor;
+        _currentAmbientColor = _nightAmbientColor;
+        _sun.intensity = _nightLightIntensity;
+        transform.rotation = Quaternion.Euler(_nightSunXAngle2Dusk, transform.rotation.y, transform.rotation.z);
+        _lerpTimer = 0f;
+    }
+
+
 
     private float EulerAngleToClockDegree(float angle)
     {
